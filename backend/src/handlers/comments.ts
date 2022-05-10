9c4efb8a8cdcd40eb03b390c71cc061f1aa7f200
Avin <avinkavesha@gmail.com>
2022-05-10 20:32:38 +0530

    feat: add userId to mock comments

--- backend/src/handlers/comments.ts
+++ backend/src/handlers/comments.ts
@@ -11,18 +11,21 @@ export function configureComments({ app }: { app: FastifyInstance }) {
       id: 1,
       body: `Jeepers now that's a huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.`,
       upvotes: 0,
+      userId: 1,
       createdAt: subMinutes(new Date, 45)
     },
     {
       id: 2,
       body: `Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious. :)`,
       upvotes: 5,
+      userId: 2,
       createdAt: subDays(new Date, 1)
     },
     {
       id: 3,
       body: `Love the native memberships and the zipless themes, I was just asked by a friend about options for a new site, and I think I know what I'll be recommending then...`,
       upvotes: 0,
+      userId: 3,
       createdAt: subWeeks(new Date, 3)
     }
   ]
@@ -34,7 +37,7 @@ export function configureComments({ app }: { app: FastifyInstance }) {
     }
   })
 
-  type Request = FastifyRequest<{ Body: { body: string } }>
+  type Request = FastifyRequest<{ Body: { body: string, userId: number } }>
 
   app.post('/api/v1/comments', (request: Request, reply) => {
     const comment = { id: nextId(), ...request.body, upvotes: 0, createdAt: new Date() }
