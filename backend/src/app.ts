b40bc5b5a0d4cb4d90f25ba16658007b43eede3e
Avin <avinkavesha@gmail.com>
2022-05-10 17:27:21 +0530

    refactor: move comments handlers to own file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import { addDays, subDays, subHours, subMinutes, subWeeks } from 'date-fns'
 import Fastify, { FastifyRequest } from 'fastify'
+import { configureComments } from "./handlers/comments"
 
 const app = Fastify({
   logger: true
@@ -10,51 +10,6 @@ app.register(require('@fastify/cors'), {
   origin: true
 })
 
-
-let id = 4
-
-const nextId = () => id++
-
-const sampleComments = [
-  {
-    id: 1,
-    body: `Jeepers now that's a huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.`,
-    upvotes: 0,
-    createdAt: subMinutes(new Date, 45)
-  },
-  {
-    id: 2,
-    body: `Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious. :)`,
-    upvotes: 5,
-    createdAt: subDays(new Date, 1)
-  },
-  {
-    id: 3,
-    body: `Love the native memberships and the zipless themes, I was just asked by a friend about options for a new site, and I think I know what I'll be recommending then...`,
-    upvotes: 0,
-    createdAt: subWeeks(new Date, 3)
-  }
-]
-
-app.get('/api/v1/comments', async (request, reply) => {
-
-  return {
-    data: sampleComments
-  }
-})
-
-type Request = FastifyRequest<{ Body: { body: string } }>
-
-app.post('/api/v1/comments', (request: Request, reply) => {
-  const comment = { id: nextId(), ...request.body, upvotes: 0, createdAt: new Date() }
-
-  sampleComments.push(comment)
-
-  reply.status(201).send({ data: comment })
-})
-
-app.post('/api/v1/comments/:id/upvote', (request, reply) => {
-
-})
+configureComments({ app })
 
 export default app
