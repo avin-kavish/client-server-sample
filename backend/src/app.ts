13918dafbcc5789db06c65b234ee2fd160231320
Avin <avinkavesha@gmail.com>
2022-05-10 16:23:48 +0530

    feat(backend): add createdAt date to mock comments

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import { addDays, subDays, subHours, subMinutes, subWeeks } from 'date-fns'
 import Fastify, { FastifyRequest } from 'fastify'
 
 const app = Fastify({
@@ -18,17 +19,20 @@ const sampleComments = [
   {
     id: 1,
     body: `Jeepers now that's a huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.`,
-    upvotes: 0
+    upvotes: 0,
+    createdAt: subMinutes(new Date, 45)
   },
   {
     id: 2,
     body: `Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious. :)`,
-    upvotes: 5
+    upvotes: 5,
+    createdAt: subDays(new Date, 1)
   },
   {
     id: 3,
     body: `Love the native memberships and the zipless themes, I was just asked by a friend about options for a new site, and I think I know what I'll be recommending then...`,
-    upvotes: 0
+    upvotes: 0,
+    createdAt: subWeeks(new Date, 3)
   }
 ]
 
@@ -42,7 +46,7 @@ app.get('/api/v1/comments', async (request, reply) => {
 type Request = FastifyRequest<{ Body: { body: string } }>
 
 app.post('/api/v1/comments', (request: Request, reply) => {
-  const comment = { id: nextId(), ...request.body, upvotes: 0 }
+  const comment = { id: nextId(), ...request.body, upvotes: 0, createdAt: new Date() }
 
   sampleComments.push(comment)
 
