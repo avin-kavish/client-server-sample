63b872b94d685ec58a019c2b76342c539644f59a
Avin <avinkavesha@gmail.com>
2022-05-13 10:44:25 +0530

    feat(backend): add dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import Fastify, { FastifyRequest } from 'fastify'
 import { configureComments } from "./handlers/comments"
 import { configureUpvotes } from "./handlers/upvotes"
