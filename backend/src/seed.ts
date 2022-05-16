726bf98f09531582704c1a2faa02add8c912bdcf
Avin <avinkavesha@gmail.com>
2022-05-16 22:31:50 +0530

    refactor: use autoincrementing id

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -10,22 +10,20 @@ import { prisma } from "./prisma/client"
   await prisma.user.createMany({
     data: [
       {
-        id: 1,
         name: 'Rob Hope',
         avatar: 'images/avatar-bob.png'
       },
       {
-        id: 2,
         name: 'Sophie Brecht',
         avatar: 'images/avatar-sophie.png'
       },
       {
-        id: 3,
         name: 'Cameron Lawrence',
         avatar: 'images/avatar-cameron.png'
       },
     ]
   })
+  const users = await prisma.user.findMany({ orderBy: { id: 'asc' } })
 
   await prisma.comment.createMany({
     data: [
@@ -33,31 +31,32 @@ import { prisma } from "./prisma/client"
         id: 1,
         body: `Jeepers now that's a huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.`,
         upvoteCount: 0,
-        userId: 1,
+        userId: users[0].id,
         createdAt: subMinutes(new Date, 45)
       },
       {
         id: 2,
         body: `Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious. :)`,
         upvoteCount: 5,
-        userId: 2,
+        userId: users[1].id,
         createdAt: subDays(new Date, 1)
       },
       {
         id: 3,
         body: `Love the native memberships and the zipless themes, I was just asked by a friend about options for a new site, and I think I know what I'll be recommending then...`,
         upvoteCount: 0,
-        userId: 3,
+        userId: users[2].id,
         createdAt: subWeeks(new Date, 3)
       }
     ]
   })
+  const comments = await prisma.comment.findMany({ orderBy: { id: 'asc' } })
 
   await prisma.upvote.createMany({
     data: [
       {
-        commentId: 2,
-        userId: 1,
+        commentId: comments[1].id,
+        userId: users[0].id,
       }
     ]
   })
