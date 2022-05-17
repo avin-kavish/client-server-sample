59b81c9763423fd272145c8d918d812c5811a0ce
Avin <avinkavesha@gmail.com>
2022-05-17 20:27:44 +0530

    feat(backend): listen on port 3010

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import app from "./app"
 
 const start = async () => {
   try {
-    await app.listen(3000)
+    await app.listen(3010)
   } catch (err) {
     app.log.error(err)
     process.exit(1)
