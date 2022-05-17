7a9ceb4c1cd23e43fffd6c341de66e80a70809c2
Avin <avinkavesha@gmail.com>
2022-05-17 21:20:06 +0530

    feat(web-app): migrate upvote button

--- web-app/src/pages/CommentsPage/CommentsPage.tsx
+++ web-app/src/pages/CommentsPage/CommentsPage.tsx
@@ -2,10 +2,18 @@ import { formatDistanceToNow } from "date-fns"
 import React, { useMemo } from "react"
 import useSWR from "swr"
 import { Comment, Upvote, User } from "../../lib/types"
+import { fetchJson } from "../../lib/utils"
 
 const currentUser = 1
 
-function renderComment(c: Comment, user: User | undefined, upvote: Upvote | undefined) {
+interface CommentProps {
+  comment: Comment
+  user: User | undefined
+  upvote: Upvote | undefined
+  onUpvote: (commentId: number) => void
+}
+
+function CommentItem({ comment, upvote, user, onUpvote }: CommentProps) {
 
   return (
     <div className="comments__comment-container">
@@ -23,22 +31,21 @@ function renderComment(c: Comment, user: User | undefined, upvote: Upvote | unde
             ・
           </div>
           <div className="comments__comment-time">
-            {formatDistanceToNow(new Date(c.createdAt), { addSuffix: true })}
+            {formatDistanceToNow(new Date(comment.createdAt), { addSuffix: true })}
           </div>
         </div>
         <div className="comments__comment-content-body">
-          {c.body}
+          {comment.body}
         </div>
         <div className="comments__comment-actions">
-          <button className="button button-text upvote-button" data-comment-id={c.id}>
+          <button
+            className="button button-text upvote-button"
+            onClick={() => onUpvote(comment.id)}
+          >
             {upvote ? `Upvoted` : `▲ Upvote`}
           </button>
-          <div
-            className="comments__comment-upvotes"
-            data-comment-id={c.id}
-            data-upvotes={c.upvoteCount}
-          >
-            {c.upvoteCount} upvotes
+          <div className="comments__comment-upvotes">
+            {comment.upvoteCount} upvotes
           </div>
           {/*<button className="button button-text reply-button">*/}
           {/*  Reply*/}
@@ -50,8 +57,11 @@ function renderComment(c: Comment, user: User | undefined, upvote: Upvote | unde
 }
 
 export default function CommentsPage() {
-  const { data: comments } = useSWR<Comment[]>('/v1/comments')
-  const { data: upvotes } = useSWR<Upvote[]>(`/v1/upvotes?userId=${currentUser}`)
+  const { data: comments, mutate: mutateComments } = useSWR<Comment[]>('/v1/comments')
+  const {
+    data: upvotes,
+    mutate: mutateUpvotes
+  } = useSWR<Upvote[]>(`/v1/upvotes?userId=${currentUser}`)
 
   const params = useMemo(() => comments
     ? new URLSearchParams([
@@ -61,6 +71,41 @@ export default function CommentsPage() {
     : null, [ comments ])
 
   const { data: users } = useSWR<User[]>(params ? `/v1/users?${params}` : null)
+
+  const onUpvote = async (commentId: number) => {
+    await mutateUpvotes(async upvotes => {
+      if (!upvotes) return
+
+      const upvoteIdx = upvotes.findIndex(u => u.commentId === commentId)
+
+      await mutateComments(comments => {
+        if (!comments) throw new Error('Logic Error')
+
+        const comment = comments!.find(c => c.id === commentId)
+        if (!comment) throw new Error('Logic Error')
+
+        if (upvoteIdx === -1)
+          comment.upvoteCount++
+        else
+          comment.upvoteCount--
+
+        console.log(comment)
+
+        return [ ...comments ]
+      })
+
+      if (upvoteIdx === -1) {
+        const newUpvote = { userId: currentUser, commentId }
+        await fetchJson(`/v1/comments/${commentId}/upvotes`, newUpvote, 'POST')
+
+        return [ ...upvotes!, newUpvote ]
+      } else {
+        await fetchJson(`/v1/comments/${commentId}/upvotes?userId=${currentUser}`, undefined, 'DELETE')
+
+        return upvotes.filter((u, idx) => idx !== upvoteIdx)
+      }
+    })
+  }
 
   return (
     <div className="page-container">
@@ -83,7 +128,15 @@ export default function CommentsPage() {
           const user = users?.find(u => u.id === c.userId)
           const upvote = upvotes?.find(u => u.commentId === c.id)
 
-          return renderComment(c, user, upvote)
+          return (
+            <CommentItem
+              key={c.id}
+              comment={c}
+              upvote={upvote}
+              user={user}
+              onUpvote={onUpvote}
+            />
+          )
         })}
       </div>
     </div>
